072674b150de18262f5e0905ac96e9477718c24b
Will Carter <will.carter@me.com>
2014-05-08 23:08:17 +0100

    5404aab fix deply

---
+++ scripts/social.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#social').socialist({
+        networks: [
+            {name:'linkedin',id:'buddy-media'},
+            {name:'facebook',id:'facebook'},
+            {name:'pinterest',id:'potterybarn'}
+        ]
+    });
+});
